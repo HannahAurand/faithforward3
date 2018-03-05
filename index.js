4272fe82fcb7ad82c6c442667f8d37d098d5c114
“Hannah <hannahaurand@Hannahs-MacBook-Pro.local>
2018-03-05 10:05:32 -0500

    Add console log to check that it is working with nodemon, utilize hbs as view engine and use parser urlencoded

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const parser = require("body-parser");
 const passport = require("passport");
 const cookie = require("cookie-parser");
 const bcrypt = require("bcrypt");
+
+app.set("view-engine", "hbs");
+app.use(parser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Funciona!");
+});
