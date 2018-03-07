10ef17fb0ef5a70def62ee83306c05b4d4f1d6e7
“Hannah <hannahaurand@Hannahs-MacBook-Pro.local>
2018-03-07 11:06:02 -0500

    Add different styling. Change image, display to flex in body, and opacity of events.

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const mongo = require("mongo");
 const hbs = require("hbs");
 const parser = require("body-parser");
 const passport = require("passport");
-// const
-// const cookie = require("cookie-parser");
-// const bcrypt = require("bcrypt");
 const Event = require("./models/Event");
 const eventsController = require("./controllers/event");
 const methodOverride = require("method-override");
@@ -16,13 +13,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "hbs");
 app.use(parser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(parser());
 
-//Passport stuff
-// require("./config/passport")(passport);
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(cookieParser());
-// app.use(bodyParser());
 app.get("/", (req, res) => {
   Event.find({}).then(events => {
     res.render("index", { events });
